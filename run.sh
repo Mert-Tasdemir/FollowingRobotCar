e4dab14fb328a236533704817717c49f2d2c1fe0
MERT T <mertas2002@gmail.com>
2025-05-08 22:10:53 +0300

    fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /home/mert/source/FollowingRobotCar
-ping 127.0.0.1 -4  -w 7 -q
+cd /home/mert/source/FollowingRobotCar/
+ping 127.0.0.1 -4  -w 1 -q
 screen -dmS car python carMoveControlled2.py
+# python carMoveControlled2.py
 
